1d88836ece6988f8b068744d531e8df0a9aa0103
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-02-10 14:13:56 -0300

    Update user model

--- Simple.Coinos/Models/UserModels.cs
+++ Simple.Coinos/Models/UserModels.cs
@@ -15,6 +15,12 @@ public class UserInfo
     public bool fiat { get; set; }
     public string id { get; set; }
     public int locktime { get; set; }
+    public string npub { get; set; }
+    public string nsec { get; set; }
+    public string nwc { get; set; }
+    public string pubkey { get; set; }
+    public string sk { get; set; }
+    public string token { get; set; }
     public string username { get; set; }
 }
 
